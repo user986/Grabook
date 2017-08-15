b4310f89211c2e776088cf170ea0023f343cdfe2
Ashish Srivastava <ashish.srivastava@icreon.com>
2017-08-15 23:22:30 +0530

    vendor files

---
+++ grabookapi/vendor/spatie/laravel-fractal/src/FractalServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Spatie\Fractal;
+
+use Illuminate\Support\ServiceProvider;
+use Spatie\Fractal\Console\Commands\TransformerMakeCommand;
+
+class FractalServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../resources/config/laravel-fractal.php' => config_path('laravel-fractal.php'),
+        ], 'config');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                TransformerMakeCommand::class,
+            ]);
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../resources/config/laravel-fractal.php', 'laravel-fractal');
+
+        $this->app->singleton('laravel-fractal', function ($arguments) {
+            return fractal($arguments);
+        });
+
+        $this->app->alias('laravel-fractal', Fractal::class);
+    }
+}
